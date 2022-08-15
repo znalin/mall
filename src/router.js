07249f72a7ee02f02fb6cf0dab88e3a1cfad9fe4
张媚琳 <zhangmeilin@vision-medtech.com>
2022-08-15 17:34:39 +0800

    性能优化

--- src/router.js
+++ src/router.js
@@ -3,23 +3,15 @@
  * @Author: znalin
  * @Date: 2022-07-13 15:06:18
  * @LastEditors: znalin
- * @LastEditTime: 2022-08-09 17:43:11
+ * @LastEditTime: 2022-08-15 16:23:18
  */
 import Vue from 'vue'
-import vueRouter from 'vue-router'
+import Router from 'vue-router'
 import Home from './pages/home'
 import Index from './pages/index'
-import Product from './pages/product'
-import Detail from './pages/detail'
-import Cart from './pages/cart'
-import Order from './pages/order'
-import OrderConfirm from './pages/orderConfirm'
-import OrderList from './pages/orderList'
-import OrderPay from './pages/orderPay'
-import Alipay from './pages/alipay'
-import Login from './pages/login'
-Vue.use(vueRouter)
-export default new vueRouter({
+Vue.use(Router);
+
+export default new Router({
   routes:[
     {
       path:'/',
@@ -28,64 +20,61 @@ export default new vueRouter({
       redirect:'/index',
       children:[
         {
-          path:'/index',
-          name:'index',
-          component:Index,
-        },
-        {
-          path:'/product/:id',
-          name:'product',
-          component:Product,
-        },
-        {
-          path:'/detail/:id',
-          name:'detail',
-          component:Detail,
+          path: '/index',
+          name: 'index',
+          component: Index,
+        }, {
+          path: '/product/:id',
+          name: 'product',
+          component: () => import('./pages/product.vue')
+        }, {
+          path: '/detail/:id',
+          name: 'detail',
+          component: () => import('./pages/detail.vue')
         }
-
       ]
     },
     {
-      path:'/login',
-      name:'login',
-      component:Login,
-     
+      path: '/login',
+      name: 'login',
+      component: () => import('./pages/login.vue')
     },
     {
-      path:'/cart',
-      name:'cart',
-      component:Cart,
-     
+      path: '/register',
+      name: 'register',
+      component: () => import('./pages/register.vue')
     },
     {
-      path:'/order',
-      name:'order',
-      component:Order,
+      path: '/cart',
+      name: 'cart',
+      component: () => import('./pages/cart.vue')
+    },
+    {
+      path: '/order',
+      name: 'order',
+      component: () => import('./pages/order.vue'),
       children:[
         {
-          path:'list',
-          name:'order-list',
-          component:OrderList,
+          path: 'list',
+          name: 'order-list',
+          component: () => import('./pages/orderList.vue')
         },
         {
-          path:'confirm',
-          name:'order-confirm',
-          component:OrderConfirm,
+          path: 'confirm',
+          name: 'order-confirm',
+          component: () => import('./pages/orderConfirm.vue')
         },
         {
-          path:'pay',
-          name:'order-pay',
-          component:OrderPay,
+          path: 'pay',
+          name: 'order-pay',
+          component: () => import('./pages/orderPay.vue')
         },
         {
-          path:'alipay',
-          name:'alipay',
-          component:Alipay,
+          path: 'alipay',
+          name: 'alipay',
+          component: () => import('./pages/alipay.vue')
         }
-
       ]
-     
     }
   ]
-  
-})
+});
